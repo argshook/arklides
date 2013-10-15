0e770f6b2b825a0b469f3ca6b9d466000c8061a5
Arijus Šukys <argshook@gmail.com>
2013-10-16 02:15:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Three questions quiz game with horses or something.
+
+A friend of mine needed this.
+
+## Adding more questions
+
+Add your own questions to `Game.questions` array like so:
+
+    [	
+	"What is the answer to everything?", // Question text
+	"1", 				     // First option
+	"69",				     // Second option
+	"42",				     // Third option
+	3,				     // The correct option (the answer)
+	"horse.jpg"			     // Question image path (optional). Question images should be hosted at /assets/images
+    ]
+
+ 
---
+++ js/index.js
@@ -0,0 +1,121 @@
+var Game = {
+  questions: [
+    ["Ar pienas baltas?", "Taip", "Ne", "Nežinau", 1, "arklys.jpg"],
+    ["Ką geria karvė?", "Pieną", "Alų", "Brendį", 1]
+  ],
+  
+  // Elements
+  el: {
+    "question": document.getElementById("question"),
+    "answers": document.getElementById("answers"),
+    "answerOne": document.getElementById("answerOne"),
+    "answerTwo": document.getElementById("answerTwo"),
+    "answerThree": document.getElementById("answerThree"),
+    "note": document.getElementById("note"),
+    "gameOver": document.getElementById("gameOver"),
+    "playAgain": document.getElementById("playAgain"),
+    "score": document.getElementById("score")
+  },
+  init: function() {
+    this.currentQuestion = "";
+    this.currentAnswer = 0;
+    this.score = 0;
+    this.el.score.innerHTML = "0";
+    
+    this.assets = {};
+    
+    this.assets.images = "assets/images";
+    
+    this.start();
+  },
+  start: function() {
+    if(this.currentQuestion === "") {
+      // display the question and populate answers
+      this.generateQuestion(this.questions);
+           
+    } else {
+      this.el.question.innerHTML = "?";
+    }
+  },
+  restart: function() {
+    this.currentQuestion = "";
+    this.currentAnswer = 0;
+    this.el.gameOver.style.display = "none";
+    this.el.note.innerHTML = "";
+    this.start();
+  },
+  generateQuestion: function(questions) {
+    /* questions[question][0] - the question
+       questions[question][1] - the first answer
+       questions[question][2] - the second answer
+       questions[question][3] - the third answer
+       questions[question][4] - the correct answer (number 1, 2 or 3)
+       questions[question][5] - the image to go along with the question (optional)
+    */
+    
+    if(questions.length > 0) {
+      var random = this.getRandInRange(0, questions.length),
+          question = this.questions[random];
+      
+      this.currentQuestion = question;
+      this.populateAnswers(random, this.questions[random][4]);
+      
+      if(question.length === 6) {
+        return this.el.question.innerHTML = '<img src="'+this.assets.images+'/'+question[5]+'" />'+question[0];
+        //return this.el.question.innerHTML = '<img src="http://placekitten.com/300/400" />'+question[0];
+      }
+      
+      return this.el.question.innerHTML = question[0];
+    } else {
+      this.el.questionInnerHTML = "Nėra klausimų :(";
+    }
+  },
+  populateAnswers: function(question, answer) {
+    this.currentAnswer = answer;
+    this.el.answerOne.innerHTML = this.questions[question][1];
+    this.el.answerTwo.innerHTML = this.questions[question][2];
+    this.el.answerThree.innerHTML = this.questions[question][3];
+  },
+  checkAnswer: function() {
+    // this is not good, better fix it
+    var currentAnswer = Game.currentAnswer;
+    if (this.id === "answerOne" && currentAnswer === 1) {
+      return Game.correct(this.id);
+    } else if (this.id === "answerTwo" && currentAnswer === 2) {
+      return Game.correct(this.id);
+    } else if (this.id === "answerThree" && currentAnswer === 3) {
+      return Game.correct(this.id);
+    } else {
+      return Game.inCorrect(this.id);
+    }
+  },
+  correct: function() {
+    this.el.note.innerHTML = "Teisingai!";
+    this.el.score.innerHTML = ++this.score;
+    this.generateQuestion(this.questions);
+  },
+  inCorrect: function() {
+    this.el.gameOver.style.display = "block";
+    this.el.note.innerHTML = "Neteisingai :(";
+    this.el.playAgain.onclick = function() {
+      Game.restart();
+    };
+  },
+  
+  // Helper functions
+  getRandInRange: function(min, max) {
+    return Math.random() * (max - min) + min | 0;
+  }
+
+};
+
+window.onload = function() {
+  
+  // listen to answers being clicked and check if it's correct
+  Game.el.answerOne.addEventListener("click", Game.checkAnswer, false);
+  Game.el.answerTwo.addEventListener("click", Game.checkAnswer, false);
+  Game.el.answerThree.addEventListener("click", Game.checkAnswer, false);
+  
+  
+  Game.init();
+};
